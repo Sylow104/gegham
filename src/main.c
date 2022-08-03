64e541e1ca4c3649b6996c32a40a8f688dc496ac
Patrick Ching <burning.codex@gmail.com>
2022-08-03 10:34:29 -0600

    added procedures

--- src/main.c
+++ src/main.c
@@ -7,7 +7,11 @@ int main(int argc, const char *argv[])
 		exit(-1);
 	}
 	// main procedures here
+	table_t *to_op = table_src_import(&file->workbook.sheets[0]);
+	table_select_all_columns(to_op, -1);
+	table_build(to_op, 0x0, "fish");
 
 	// cleanup here
+	table_destroy(to_op);
 	return xlsx_close(file);
 }
