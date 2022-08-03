f0f93930dfb84e7a11d4ee391d0d3da3ba552ac2
Patrick Ching <burning.codex@gmail.com>
2022-08-03 10:34:23 -0600

    declarations for destroy and select all

--- include/gegham.h
+++ include/gegham.h
@@ -31,8 +31,9 @@ typedef struct table
 	size_t cursor;
 } table_t;
 table_t *table_src_import(sheet_t *input);
+int table_destroy(table_t *obj);
 int table_select_column(table_t *to_mod, size_t index, bool is_pk);
-int table_select_all_columns(table_t *to_mod, size_t pk_index);
+int table_select_all_columns(table_t *to_mod, ssize_t pk_index);
 int table_has_header(table_t *to_mod, bool option);
 int table_build(table_t *to_mod, sqlite3 *db, const char *tbl_name);
 
